aef5640b99620a3adaf46d0999eb8653e5ba183d
erfan-daei <erfandaei66@gmail.com>
2025-12-29 21:28:26 +0330

    Add_Creator_For_Success_And_Failed

--- Src/Common/Output/ResultDto.cs
+++ Src/Common/Output/ResultDto.cs
@@ -9,6 +9,14 @@ namespace Common.Output
         public string? Message { get; set; }
         public HttpStatusCode StatusCode { get; set; }
         public T? Data { get; set; }
+
+        //successful creator method
+        public static ResultDto<T> Succeeded(T data, string? message, HttpStatusCode httpStatusCode)
+            => new() { Data = data, IsSuccess = true, Message = message, StatusCode = httpStatusCode };
+
+        //failed creator method
+        public static ResultDto<T> Failed(string? message, HttpStatusCode httpStatusCode)
+            => new() { Data = default, IsSuccess = false, Message = message, StatusCode = httpStatusCode };
     }
 
     ////centeral "non" generic dto for result output of Application and Presentation and Persistence
@@ -17,6 +25,14 @@ namespace Common.Output
         public bool IsSuccess { get; set; } = false;
         public string? Message { get; set; }
         public HttpStatusCode StatusCode { get; set; }
+
+        //successful creator method
+        public static ResultDto Succeeded(string? message, HttpStatusCode httpStatusCode)
+            => new() { IsSuccess = true, Message = message, StatusCode = httpStatusCode };
+
+        //failed creator method
+        public static ResultDto Failed(string? message, HttpStatusCode httpStatusCode)
+            => new() { IsSuccess = false, Message = message, StatusCode = httpStatusCode };
     }
 
 }
