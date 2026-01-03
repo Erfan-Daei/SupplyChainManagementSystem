78864aa46d351adac11749670a3a50eb8fe57004
erfan-daei <erfandaei66@gmail.com>
2026-01-03 22:38:52 +0330

    Add_LogIn_IntegrationTest

---
+++ Test/InegrationTest/Presentation_Test/Services/Controllers/Area/User/UserManagement/Auth/LogInTest.cs
@@ -0,0 +1,126 @@
+﻿using Application.Interfaces.Database.ServiceRepository.Commands.UserManagementRepository;
+using Application.Interfaces.Database.ServiceRepository.Querries.UserManagementRepository;
+using Application.MediatR.Services.Commands.LogIn;
+using Domain.Entities.Common;
+using Domain.Entities.UserManagement;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Persistence.DatabaseManagement.DatabaseConfiguration;
+using Presentation.Output.Area.User.UserManagement;
+using Presentation.Output.Base;
+using Presentation_Test.Setup.Database;
+using Presentation_Test.Setup.Factory;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Presentation_Test.Services.Controllers.Area.User.UserManagement.Auth
+{
+    public class LogInTest : IClassFixture<WebApplicationFactory_Test>
+    {
+        private readonly WebApplicationFactory_Test _factory;
+        private readonly HttpClient _client;
+        private readonly IMediator _mediator;
+        private readonly DatabaseContext _db;
+        public LogInTest(WebApplicationFactory_Test factorty)
+        {
+            _factory = factorty;
+            _client = _factory.CreateClient();
+            var scope = _factory.Services.CreateScope();
+            _mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+            _db = new SeedLoginData().SetSeedLoginData(scope);
+        }
+
+        [Fact]
+        public async Task LogIn_Successfull()
+        {
+            //arrange
+            var request = new LogInCommand("Test@gmail.com", "12345Ed@");
+
+            //act
+            var response = await _client.PostAsJsonAsync("/api/User/UserManagement/Auth/LogIn", request);
+            var result = await response.Content.ReadFromJsonAsync<ApiResultDto<ApiJwtTokenResultDto>>();
+
+            //assert
+            Assert.True(result!.IsSuccess);
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            Assert.NotEmpty(result.Data!.AccessToken);
+
+            var user = _db.Users.FirstOrDefault();
+            var refreshToken = _db.UserTokens.Single(ut => ut.UserId == user.UserId && ut.UserTokenType == UserTokenType.RefreshToken.ToString());
+            Assert.NotNull(refreshToken);
+
+            var refreshTokenCookie = response.Headers.GetValues("Set-Cookie").First(c => c.StartsWith("RefreshToken"));
+            Assert.NotNull(refreshTokenCookie);
+        }
+
+        [Fact]
+        public async Task LogIn_FluentValidation_Null_Input()
+        {
+            //arrange
+            var request = new LogInCommand("WeongEmail", string.Empty);
+
+            //act
+            var response = await _client.PostAsJsonAsync("/api/User/UserManagement/Auth/LogIn", request);
+            var problems = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+
+            //assert
+            Assert.False(response.IsSuccessStatusCode);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+
+            Assert.True(problems!.Errors.ContainsKey("UserEmail"));
+            Assert.True(problems!.Errors.ContainsKey("UserPassword"));
+
+            Assert.Equal("لطفا ایمیل خود را به درستی وارد کنید", problems.Errors["UserEmail"].First());
+            Assert.Equal("لطفا رمز عبور خودرا وارد کنکید", problems.Errors["UserPassword"].First());
+        }
+
+        [Fact]
+        public async Task LogIn_Email_Not_Exist_Error()
+        {
+            //arrange
+            var request = new LogInCommand("wrong@gmail.com", "password");
+
+            //act
+            var result = await _mediator.Send(request);
+
+            //assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal("کاربر یافت نشد", result.Message);
+            Assert.Equal(HttpStatusCode.NotFound, result.StatusCode);
+            Assert.Null(result.Data);
+        }
+
+        [Fact]
+        public async Task LogIn_Wrong_Password_Error()
+        {
+            //arrange
+            var request = new LogInCommand("Test@gmail.com", "wrongPassword");
+
+            //act
+            var result = await _mediator.Send(request);
+
+            //assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal("رمز عبور اشتباه است", result.Message);
+            Assert.Equal(HttpStatusCode.Unauthorized, result.StatusCode);
+            Assert.Null(result.Data);
+        }
+
+        [Fact]
+        public async Task LogIn_Argument_Null_Exception()
+        {
+            //arrange
+            var request = new LogInCommand("Test@gmail.com", null!);
+
+            //act
+            var result = await _mediator.Send(request);
+
+            //assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal(HttpStatusCode.InternalServerError, result.StatusCode);
+            Assert.Null(result.Data);
+        }
+    }
+}
---
+++ Test/InegrationTest/Presentation_Test/Setup/Database/SeedLoginData.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities.Common;
+using Domain.Entities.UserManagement;
+using Infrastructure.Hashing;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.DatabaseManagement.DatabaseConfiguration;
+
+namespace Presentation_Test.Setup.Database
+{
+    public class SeedLoginData
+    {
+        public DatabaseContext SetSeedLoginData(IServiceScope scope)
+        {
+            var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+
+            var password = "12345Ed@";
+            var hashedPassword = new HashManagerService().BCryptHashPassword(password);
+            var user = Domain.Entities.UserManagement.User.Create("Test", "Test@gmail.com", hashedPassword, SeedCompanies.DefaultCompanyId);
+            db!.Users.Add(user);
+
+            var userInRole = UserInRole.Create(user.UserId, SeedRoles.ViewerId);
+            db.UserInRoles.Add(userInRole);
+
+            db.SaveChanges();
+
+            return db;
+        }
+    }
+}
